b9206278eaad4ac7662ce88de9d64b076a9dfb12
Guilherme Figueiroa <guilherme.mfigueiroa@gmail.com>
2015-12-05 12:54:15 -0200

    Fix for confusion matrix

--- kNN_main.m
+++ kNN_main.m
@@ -4,25 +4,25 @@
 %Y = iris(:,5:5);
 
 % Load tf-idf sparse table
-%X = load('tfidfSparse15-1000.txt');
-%X = spconvert(X);
+X = load('tfidfSparse15-1000.txt');
+X = spconvert(X);
 
 % Load classes
-%Y = load('classes15-1000.txt');
+Y = load('classes15-1000.txt');
 
-%disp('Aplicando holdout');
-%[ training_data, training_classes, testing_data, testing_classes ] = holdout( X,Y );
+disp('Aplicando holdout');
+[ training_data, training_classes, testing_data, testing_classes ] = holdout( X,Y );
 
-%conf_matrix = zeros(20,20);
-%accuracy = zeros(length(testing_data),1);
+conf_matrix = zeros(20,20);
+accuracy = zeros(size(testing_data,1),1);
 
 disp('Classificando dados');
-for i=2632:length(testing_data)
+for i=1:size(testing_data,1)
     i
-    class = kNN(training_data,training_classes,testing_data(i,:), 100)
+    class = kNN(training_data,training_classes,testing_data(i,:), 40)
     
     accuracy(i) = class == testing_classes(i);
-    conf_matrix(class,testing_classes(i)) = conf_matrix(class,testing_classes(i))+1;
+    conf_matrix(testing_classes(i),class) = conf_matrix(testing_classes(i),class)+1;
     
     sum(accuracy(1:i))/i
 end
